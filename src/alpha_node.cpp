3ade271728c080091991fac7aae04820a3634d4c
Dmitry Bogdanov <dimabv@tut.by>
2013-12-13 17:02:18 +0100

    Load parameters (and other small stuff)

--- src/alpha_node.cpp
+++ src/alpha_node.cpp
@@ -8,8 +8,9 @@
 ros::Publisher alphaPublisher;
 ros::Subscriber cloudSubscriber;
 
-int cutWidth = 120;
-int cutHeight = 90;
+int cutWidth = 640;
+int cutHeight = 480;
+std::string paramPath;
 
 void callback(sensor_msgs::PointCloud2ConstPtr msg) {
     ros::WallTime before = ros::WallTime::now();
@@ -17,6 +18,11 @@ void callback(sensor_msgs::PointCloud2ConstPtr msg) {
     PointCloud cloud;
     pcl::fromROSMsg(*msg, cloud);
     ROS_INFO_STREAM("Received and converted " << cloud.width << "x" << cloud.height << " cloud");
+    if (cloud.height == 1) {
+        ROS_WARN_STREAM("Set dimensions explicitly");
+        cloud.height = 480;
+        cloud.width = 640;
+    }
 
     PointCloud::Ptr svsInput(new PointCloud(cutWidth, cutHeight));
     for (int i = 0; i < cutWidth; ++i) {
@@ -27,6 +33,10 @@ void callback(sensor_msgs::PointCloud2ConstPtr msg) {
     ROS_INFO_STREAM("Cut " << svsInput->width << "x" << svsInput->height << " cloud");
 
     SVSParams params;
+    if (paramPath.size()) {
+        params.Load(paramPath.c_str());
+    }
+
     SVSBuilder builder;
     builder.SetParams(params);
     builder.SetInputCloud(svsInput);
@@ -37,6 +47,7 @@ void callback(sensor_msgs::PointCloud2ConstPtr msg) {
     double const* alphas = builder.SVM().Alphas();
 
     svs::Alphas result;
+    result.header = msg->header;
     result.values.resize(nTrainingObjects);
     std::copy(alphas, alphas + nTrainingObjects, result.values.begin());
 
@@ -49,11 +60,13 @@ int main(int argc, char ** argv) {
     ros::NodeHandle n;
     n.getParam("/svs/cut/height", cutHeight);
     n.getParam("/svs/cut/width", cutWidth);
+    n.getParam("/svs/parampath", paramPath);
     ROS_INFO_STREAM("Cut height param is " << cutHeight);
     ROS_INFO_STREAM("Cut width param is " << cutWidth);
+    ROS_INFO_STREAM("Parameters path is " << paramPath);
 
     alphaPublisher = n.advertise<svs::Alphas>("/svs/alphas", 1);
-    cloudSubscriber = n.subscribe("/camera/rgb/points", 1, callback);
+    cloudSubscriber = n.subscribe("/camera/rgb/points", 10, callback);
 
     ros::spin();
     return 0;
